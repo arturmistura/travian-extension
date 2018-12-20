9c12108a256f2533f3f1c09f3dee762504cf16f8
Artur José Mistura Junior <arturmistura@gmail.com>
2018-12-20 20:12:31 -0200

    first commit

---
+++ farm-list.js
@@ -0,0 +1,2 @@
+//Notificar que precisa aumentar a quantidade de tropas
+$('.full').closest('td').prev().css('background-color', '#b9ff93');
---
+++ geral.js
@@ -0,0 +1,3 @@
+//Alterar a cor dos ataques
+$('.iReport2').closest('td').css('background-color', '#fdffaa');
+$('.iReport3').closest('td').css('background-color', '#ffd8d8');
