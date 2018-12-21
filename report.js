5916be70be4e4df671d5a7032e51c570d8fcc5b8
Artur Mistura <artur.mistura@trusoftware.com.br>
2018-12-21 12:29:08 -0200

    Criando mais algumas funções

--- report.js
+++ report.js
@@ -1,1 +1,20 @@
 $('.iReport15').closest('td').css('background-color', '#a3b5ff');
+
+criarTotalFarmado();
+
+function criarTotalFarmado() {
+        var totalFarmado = 0;
+        var totalCapacidade = 0;
+
+        $('.carry').each(function () {
+        var valores = $(this).prop('alt').split('/');
+        var farmado = parseInt(valores[0]);
+        var capacidade = parseInt(valores[1]);
+
+        totalFarmado += farmado;
+        totalCapacidade += capacidade;
+    });
+
+    var porcentagem = ((totalFarmado / totalCapacidade) * 100).toFixed(2);
+    $('#markAll').append('<div style="margin-top: 10px;"><img alt="57/500" src="img/x.gif" class="reportInfo carry half"> <span><label for="sAll2">' + totalFarmado + '/' + totalCapacidade + ' - ' + porcentagem + '% </label></span></div>');
+}
